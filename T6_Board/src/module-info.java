ebdc5b95754a58cbdff2792b7c31967fb210c076
xegenie <dltpwlswkdf@naver.com>
2024-09-03 18:44:23 +0900

    1

--- T6_Board/src/module-info.java
+++ T6_Board/src/module-info.java
@@ -1,6 +1,6 @@
 module T6_Board {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
+	requires lombok;
+	opens application to javafx.graphics, javafx.fxml, lombok;
 }
