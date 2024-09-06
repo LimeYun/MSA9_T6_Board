d9f346eb7d350d9b8ed8bdb2969dbd96422c2412
xegenie <dltpwlswkdf@naver.com>
2024-09-06 18:42:37 +0900

    지우고 다시

--- T6_Board/SQL/BOARD.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `t6_board`.`board` (
-  `no` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100) NOT NULL,
-  `writer` VARCHAR(100) NOT NULL,
-  `content` TEXT NULL,
-  `reg_date` TIMESTAMP NOT NULL DEFAULT now(),
-  `upd_date` TIMESTAMP NOT NULL DEFAULT now(),
-  `view` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`no`));
